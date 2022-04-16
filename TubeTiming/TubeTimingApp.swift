1fa7736124d29e6c640808eda3364536945829ee
jinlidavid <jin_li.d@etoncollege.org.uk>
2022-04-16 14:09:00 +0100

    Initial Commit

---
+++ TubeTiming/TubeTimingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TubeTimingApp.swift
+//  TubeTiming
+//
+//  Created by David Jin Li on 16/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct TubeTimingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
